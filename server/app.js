3bdca63302b2918f2747062d64f88fed7c7b9f39
Ryan M <rm.main@gmail.com>
2015-10-04 20:46:12 -0500

    Updated Styles and Layout

--- server/app.js
+++ server/app.js
@@ -13,6 +13,10 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
+app.get('/jsonData',function(req,res){
+   res.send(jsonData);
+});
+
 
 var server = app.listen(3000,function(){
    var port = server.address().port;
